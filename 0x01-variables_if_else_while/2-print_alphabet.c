c6c65ae8d914d10fdc772f1722528eecde9957ac
john Chimezie <mahzeeljohn@gmail>
2023-02-17 15:45:37 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * $ch - prints all letters of the alphabets in lowercase
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
