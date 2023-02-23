932985b1e058ffa093762ee6329f2c3b12f66f02
john Chimezie <mahzeeljohn@gmail>
2023-02-23 15:07:34 -0800

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - checks if numbers are 0 - 9
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+		_putchar('\n');
+}
